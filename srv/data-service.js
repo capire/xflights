1d1176134626e41254137c4e6831317cac182aa8
D050513 <sebastian.van.syckel@sap.com>
2026-06-18 10:00:15 +0200

    messaging.emit('FlightUpdated')

--- srv/data-service.js
+++ srv/data-service.js
@@ -1,18 +1,28 @@
 const cds = require ('@sap/cds')
-class DataService extends cds.ApplicationService { init() {
+class DataService extends cds.ApplicationService { async init() {
+
+  const messaging = await cds.connect.to ('messaging')
 
   const { Flights } = cds.entities ('sap.capire.flights')
 
   this.on ('BookingCreated', async req => {
     const { flight, date, seats = [null] } = req.data
-    await UPDATE (Flights, { flight_ID:flight, date }) 
-    .set `occupied_seats = occupied_seats + ${seats.length}`
+    const confirmed = await UPDATE (Flights, { flight_ID:flight, date })
+      .set `occupied_seats = occupied_seats + ${seats.length}`
+      .where `free_seats >= ${seats.length}`
+    if (!confirmed) req.reject('Flight is fully booked')
+
+    // messages can overtake each other -> don't propagate free seats in payload
+    messaging.emit('FlightUpdated', { flight_ID:flight, date })
   })
 
   this.on ('BookingDeleted', async (req) => {
     const { flight, date, seats = [null] } = req.data
-    await UPDATE (Flights, { flight_ID:flight, date }) 
-    .set `occupied_seats = occupied_seats - ${seats.length}`
+    await UPDATE (Flights, { flight_ID:flight, date })
+      .set `occupied_seats = occupied_seats - ${seats.length}`
+
+    // messages can overtake each other -> don't propagate free seats in payload
+    messaging.emit('FlightUpdated', { flight_ID:flight, date })
   })
 
   return super.init()
